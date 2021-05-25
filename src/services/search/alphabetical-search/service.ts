d1b94490e2cb7b908e858094c2d28fa1c6c4f4cd
caismith-ch <csmith2@companieshouse.gov.uk>
2021-05-25 08:56:58 +0100

    Fix linting issues

--- src/services/search/alphabetical-search/service.ts
+++ src/services/search/alphabetical-search/service.ts
@@ -4,9 +4,8 @@ import Resource from "../../resource";
 
 export default class AlphabeticalSearchService {
     constructor (private readonly client: IHttpClient) { }
-    public async getCompanies (companyName: string, requestId: string, 
+    public async getCompanies (companyName: string, requestId: string,
         searchBefore: string | null, searchAfter: string | null, size: number | null): Promise<Resource<CompaniesResource>> {
-
         const SEARCH_BEFORE_QUERY = "&search_before=";
         const SEARCH_AFTER_QUERY = "&search_after=";
         const SIZE_QUERY = "&size=";
@@ -14,18 +13,18 @@ export default class AlphabeticalSearchService {
             "X-Request-ID": requestId,
             "Content-Type": "application/json"
         }
-        const alphabeticalSearchURL = "/alphabetical-search/companies?q=" + companyName;
+        let alphabeticalSearchURL = "/alphabetical-search/companies?q=" + companyName;
 
         if (searchBefore != null) {
-            alphabeticalSearchURL + SEARCH_BEFORE_QUERY + searchBefore;
+            alphabeticalSearchURL += SEARCH_BEFORE_QUERY + searchBefore;
         }
 
         if (searchAfter != null) {
-            alphabeticalSearchURL + SEARCH_AFTER_QUERY + searchAfter;
+            alphabeticalSearchURL += SEARCH_AFTER_QUERY + searchAfter;
         }
 
         if (size != null) {
-            alphabeticalSearchURL + SIZE_QUERY + size;
+            alphabeticalSearchURL += SIZE_QUERY + size;
         }
 
         const resp = await this.client.httpGet(alphabeticalSearchURL, additionalHeaders);
