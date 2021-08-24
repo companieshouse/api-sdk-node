6a4ff7594160ee3ee831c719430c50a128153f56
svarallo <svarallo@companieshouse.gov.uk>
2021-08-24 14:28:21 +0100

    ncs-634-adding-get-transaction-call-to-transaction-service

--- src/services/transaction/service.ts
+++ src/services/transaction/service.ts
@@ -51,6 +51,41 @@ export default class TransactionService {
         return resource;
     }
 
+    public async getTransaction (transactionId: string): Promise<Resource<Transaction>|ApiErrorResponse> {
+        const url = "/transactions/" + transactionId
+        const resp = await this.client.httpGet(url);
+
+        if (resp.error) {
+            return {
+                httpStatusCode: resp.status,
+                errors: [resp.error]
+            };
+        }
+
+        const resource: Resource<Transaction> = {
+            httpStatusCode: resp.status
+        };
+
+        // cast the response body to the expected type
+        const body = resp.body as TransactionResource;
+
+        resource.resource = {
+            id: body.id,
+            etag: body.etag,
+            links: body.links,
+            reference: body.reference,
+            status: body.status,
+            kind: body.kind,
+            companyName: body.company_name,
+            companyNumber: body.company_number,
+            createdAt: body.created_at,
+            createdBy: body.created_by,
+            updatedAt: body.updated_at,
+            description: body.description
+        }
+        return resource;
+    }
+
     private mapToResource (transaction:Transaction):TransactionResource {
         return {
             company_name: transaction.companyName,
--- test/services/transaction/service.spec.ts
+++ test/services/transaction/service.spec.ts
@@ -22,7 +22,7 @@ describe("transaction", () => {
         done();
     });
 
-    it("returns an error response on failure", async () => {
+    it("returns an error response on failure (post)", async () => {
         const mockPostResponse = {
             status: 401,
             error: "An error occurred"
@@ -37,7 +37,7 @@ describe("transaction", () => {
         expect(castedData.errors[0]).to.equal("An error occurred");
     });
 
-    it("maps the company field data items correctly", async () => {
+    it("maps the company field data items correctly (post)", async () => {
         const mockResponseBody : TransactionResource = ({
             id: "12345678",
             company_name: "HELLO LTD",
@@ -57,6 +57,51 @@ describe("transaction", () => {
         const mockRequest = sinon.stub(requestClient, "httpPost").resolves(mockPostResponse);
         const transaction : TransactionService = new TransactionService(requestClient);
         const data = await transaction.postTransaction({} as Transaction);
+
+        expect(data.httpStatusCode).to.equal(200);
+        const castedData: Resource<Transaction> = data as Resource<Transaction>;
+        expect(castedData.resource.companyName).to.equal(mockResponseBody.company_name);
+        expect(castedData.resource.companyNumber).to.equal(mockResponseBody.company_number);
+        expect(castedData.resource.links.self).to.equal(mockResponseBody.links.self);
+        expect(castedData.resource.reference).to.equal(mockResponseBody.reference);
+        expect(castedData.resource.description).to.equal(mockResponseBody.description);
+    });
+
+    it("returns an error response on failure (get)", async () => {
+        const mockGetResponse = {
+            status: 401,
+            error: "An error occurred"
+        };
+
+        const mockRequest = sinon.stub(requestClient, "httpGet").resolves(mockGetResponse);
+        const transaction : TransactionService = new TransactionService(requestClient);
+        const data = await transaction.getTransaction({} as string);
+
+        expect(data.httpStatusCode).to.equal(401);
+        const castedData: ApiErrorResponse = data;
+        expect(castedData.errors[0]).to.equal("An error occurred");
+    });
+
+    it("maps the company field data items correctly (get)", async () => {
+        const mockResponseBody : TransactionResource = ({
+            id: "12345678",
+            company_name: "HELLO LTD",
+            company_number: "88",
+            links: {
+                self: "/self"
+            },
+            reference: "ref",
+            description: "desc"
+        });
+
+        const mockGetResponse = {
+            status: 200,
+            body: mockResponseBody
+        };
+
+        const mockRequest = sinon.stub(requestClient, "httpGet").resolves(mockGetResponse);
+        const transaction : TransactionService = new TransactionService(requestClient);
+        const data = await transaction.getTransaction({} as string);
 
         expect(data.httpStatusCode).to.equal(200);
         const castedData: Resource<Transaction> = data as Resource<Transaction>;
