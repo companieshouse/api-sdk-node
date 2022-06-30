919a93b13e2ddc48a29c6179582f70d87e1e5150
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2022-06-30 10:23:06 +0100

    Fix type checking error on OE test

--- test/services/overseas-entities/overseas.entities.spec.ts
+++ test/services/overseas-entities/overseas.entities.spec.ts
@@ -32,7 +32,7 @@ describe("OverseasEntityService Tests suite", () => {
         )) as Resource<OverseasEntityCreated>;
 
         expect(data.httpStatusCode).to.equal(201);
-        expect(data.resource.id).to.equal(mockValues.mockOverseasEntityCreatedResource.id);
+        expect(data.resource?.id).to.equal(mockValues.mockOverseasEntityCreatedResource.id);
     });
 
     it("should return error 401 (Unauthorised) for postOverseasEntity method", async () => {
@@ -42,7 +42,7 @@ describe("OverseasEntityService Tests suite", () => {
         const data = await oeService.postOverseasEntity(mockValues.TRANSACTION_ID, {}) as ApiErrorResponse;
 
         expect(data.httpStatusCode).to.equal(401);
-        expect(data.errors[0]).to.equal(mockValues.UNAUTHORISED);
+        expect(data.errors?.[0]).to.equal(mockValues.UNAUTHORISED);
     });
 });
 
@@ -62,11 +62,11 @@ describe("Mapping OverseasEntity Tests suite", () => {
         expect(data.presenter).to.deep.equal(mockValues.OVERSEAS_ENTITY_OBJECT_MOCK.presenter);
         expect(data.entity).to.deep.equal(mockValues.OVERSEAS_ENTITY_OBJECT_MOCK.entity);
         expect(data.beneficial_owners_statement).to.equal(mockValues.OVERSEAS_ENTITY_OBJECT_MOCK.beneficial_owners_statement);
-        expect(data.beneficial_owners_individual[0]).to.deep.equal(mockValues.OVERSEAS_ENTITY_RESOURCE_OBJECT_MOCK.beneficial_owners_individual[0]);
-        expect(data.beneficial_owners_corporate[0]).to.deep.equal(mockValues.OVERSEAS_ENTITY_RESOURCE_OBJECT_MOCK.beneficial_owners_corporate[0]);
-        expect(data.beneficial_owners_government_or_public_authority[0]).to.deep.equal(
-            mockValues.OVERSEAS_ENTITY_RESOURCE_OBJECT_MOCK.beneficial_owners_government_or_public_authority[0]);
-        expect(data.managing_officers_individual[0]).to.deep.equal(mockValues.OVERSEAS_ENTITY_RESOURCE_OBJECT_MOCK.managing_officers_individual[0]);
-        expect(data.managing_officers_corporate[0]).to.deep.equal(mockValues.OVERSEAS_ENTITY_RESOURCE_OBJECT_MOCK.managing_officers_corporate[0]);
+        expect(data.beneficial_owners_individual?.[0]).to.deep.equal(mockValues.OVERSEAS_ENTITY_RESOURCE_OBJECT_MOCK.beneficial_owners_individual?.[0]);
+        expect(data.beneficial_owners_corporate?.[0]).to.deep.equal(mockValues.OVERSEAS_ENTITY_RESOURCE_OBJECT_MOCK.beneficial_owners_corporate?.[0]);
+        expect(data.beneficial_owners_government_or_public_authority?.[0]).to.deep.equal(
+            mockValues.OVERSEAS_ENTITY_RESOURCE_OBJECT_MOCK.beneficial_owners_government_or_public_authority?.[0]);
+        expect(data.managing_officers_individual?.[0]).to.deep.equal(mockValues.OVERSEAS_ENTITY_RESOURCE_OBJECT_MOCK.managing_officers_individual?.[0]);
+        expect(data.managing_officers_corporate?.[0]).to.deep.equal(mockValues.OVERSEAS_ENTITY_RESOURCE_OBJECT_MOCK.managing_officers_corporate?.[0]);
     });
 });
