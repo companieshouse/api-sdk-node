1d1520744d8fda837d70882d27e5e2c4c73985dd
markpit <mpitten@companieshouse.gov.uk>
2022-09-16 14:44:45 +0100

    ROE-1271 POC to test save and resume ideas

--- src/services/overseas-entities/service.ts
+++ src/services/overseas-entities/service.ts
@@ -24,4 +24,42 @@ export default class OverseasEntityService {
         resource.resource = { ...response.body };
         return resource;
     }
+
+    public async putOverseasEntity (transactionId: string, body: OverseasEntity): Promise<Resource<OverseasEntityCreated> | ApiErrorResponse> {
+        const URL = `/transactions/${transactionId}/overseas-entity`;
+        const response: HttpResponse = await this.client.httpPut(URL, mapOverseasEntity(body));
+
+        if (response.error) {
+            return {
+                httpStatusCode: response.status,
+                errors: [response.error]
+            };
+        }
+
+        const resource: Resource<OverseasEntityCreated> = {
+            httpStatusCode: response.status
+        };
+
+        resource.resource = { ...response.body };
+        return resource;
+    }
+
+    // no id returned, so this needs changing
+    public async patchOverseasEntity (transactionId: string): Promise<Resource<void> | ApiErrorResponse> {
+        const URL = `/transactions/${transactionId}/overseas-entity`;
+        const response: HttpResponse = await this.client.httpPatch(URL);
+
+        if (response.error) {
+            return {
+                httpStatusCode: response.status,
+                errors: [response.error]
+            };
+        }
+
+        const resource: Resource<OverseasEntityCreated> = {
+            httpStatusCode: response.status
+        };
+
+        return resource;
+    }
 }
