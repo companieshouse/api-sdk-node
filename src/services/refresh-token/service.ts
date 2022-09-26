c4b9e6757a1bddf9f361110926d084fa7a862098
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2022-09-26 10:00:49 +0100

    Change url and add one comment

--- src/services/refresh-token/service.ts
+++ src/services/refresh-token/service.ts
@@ -11,14 +11,19 @@ export default class {
         clientId: string,
         clientSecret: string
     ): Promise<Resource<RefreshTokenData> | ApiErrorResponse> {
-        const url = `/oauth2/token`;
+        const url = `/oauth2/token?grant_type=${grantType}&refresh_token=${refreshToken}&client_id=${clientId}&client_secret=${clientSecret}`;
         const params = {
             refresh_token: refreshToken,
             grant_type: grantType,
             client_id: clientId,
             client_secret: clientSecret
         };
-        const response = await this.client.httpPost(url, params);
+
+        console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+        console.log(url);
+        console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+
+        const response = await this.client.httpPost(url, {});
 
         if (response.error) {
             return {
