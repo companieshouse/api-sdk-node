babc9a17da405d72980d7d0e544c31abf91f30c2
KALatchman <klatchman@companieshouse.gov.uk>
2023-03-30 11:50:08 +0100

    removed previous untested code

--- src/services/officer-filing/service.ts
+++ src/services/officer-filing/service.ts
@@ -11,19 +11,6 @@ import Resource, { ApiErrorResponse } from "../resource";
 export default class {
     constructor (private readonly client: IHttpClient) { }
 
-    public async postTm01 (transactionId: string,
-        tm01Submission: Tm01Submission): Promise<Resource<Tm01Submission> | ApiErrorResponse> {
-        const baseUrl = this.getOfficerFilingUrlIncTransactionId(transactionId);
-        const resp: HttpResponse =
-            await this.client.httpPost(`${baseUrl}`,tm01Submission);
-
-        const resource: Resource<String> = {
-            httpStatusCode: resp.status,
-            resource: resp.body
-        };
-        return resource;
-    }
-
     public async getListActiveDirectorDetails (transactionId: string): Promise<Resource<ActiveOfficerDetails[]> | ApiErrorResponse> {
         const url = `${this.getPrivateOfficerFilingUrlIncTransactionId(transactionId)}/active-directors-details`;
         const resp: HttpResponse = await this.client.httpGet(url);
