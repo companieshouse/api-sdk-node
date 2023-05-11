39a17ea1af2fcc472b56996af425af802451d065
jbestwickch <jbestwick@companieshouse.gov.uk>
2023-05-11 14:52:32 +0100

    Add call to TM01 to get info for TM01 check your answers page

--- src/services/officer-filing/service.ts
+++ src/services/officer-filing/service.ts
@@ -11,6 +11,41 @@ export default class {
 
     public async getListActiveDirectorDetails (transactionId: string): Promise<Resource<CompanyOfficer[]> | ApiErrorResponse> {
         const url = `${this.getOfficerFilingUrlIncTransactionId(transactionId)}/active-directors-details`;
+        return this.getCompanyOfficerDetails(url);
+    }
+
+    private getOfficerFilingUrlIncTransactionId (transactionId: string) {
+        return `/transactions/${transactionId}/officers`;
+    }
+
+    private getOfficerFilingUrlIncTransactionIdAndSubmissionId (transactionId: string, submissionId: string) {
+        return `/transactions/${transactionId}/officers/${submissionId}/`;
+    }
+
+    /**
+    * Get the director details including the termination date out of the filing.
+    * to be used on the check your answers page for TM01.
+    *
+    * @params transaction id and submission id to look up the filing
+    */
+
+    public async getDirectorAndTerminationDate (transactionId: string, submissionId: string): Promise<Resource<CompanyOfficer> | ApiErrorResponse> {
+        const url = `${this.getOfficerFilingUrlIncTransactionIdAndSubmissionId(transactionId, submissionId)}/check-answers-directors-details`;
+        return this.getCompanyOfficerDetails(url);
+    }
+
+    public async getCurrentOrFutureDissolved (companyNumber: String): Promise<Resource<Boolean> | ApiErrorResponse> {
+        const url = `/officer-filing/company/${companyNumber}/eligibility-check/past-future-dissolved`;
+        const resp: HttpResponse = await this.client.httpGet(url);
+
+        if (resp.status >= 400) {
+            return { httpStatusCode: resp.status, errors: [resp.error] };
+        }
+
+        return { httpStatusCode: resp.status, resource: resp.body as Boolean };
+    }
+
+    public async getCompanyOfficerDetails (url: string): Promise<Resource<CompanyOfficer[]> | ApiErrorResponse> {
         const resp: HttpResponse = await this.client.httpGet(url);
 
         if (resp.status >= 400) {
@@ -25,47 +60,6 @@ export default class {
 
         return resource;
     }
-
-    private getOfficerFilingUrlIncTransactionId (transactionId: string) {
-        return `/transactions/${transactionId}/officers`;
-    }
-
-    private getOfficerFilingUrlIncTransactionIdAndSubmissionId (transactionId: string, submissionId: string) {
-        return `/transactions/${transactionId}/officers/submissionId/${submissionId}/`;
-    }
-
-/**
-* Get the director details including the termination date out of the filing.
-* to be used on the check your answers page for TM01.
-*
-* @params transaction id and submission id to look up the filing
-*/
-
-    public async getDirectorAndTerminationDate (transactionId: string, submissionId: string): Promise<Resource<CompanyOfficer> | ApiErrorResponse> {
-        const url = `${this.getOfficerFilingUrlIncTransactionIdAndSubmissionId(transactionId, submissionId)}/check-answers-directors-details`;
-        const resp: HttpResponse = await this.client.httpGet(url);
-
-        if (resp.status >= 400) {
-            return { httpStatusCode: resp.status, errors: [resp.error] };
-        }
-
-        const resource: Resource<CompanyOfficer> = { httpStatusCode: resp.status };
-
-        const body = resp.body as CompanyOfficerResource;
-
-        resource.resource = Mapping.camelCaseKeys<CompanyOfficer>(body);
-
-        return resource;
-    }
-
-    public async getCurrentOrFutureDissolved (companyNumber: String): Promise<Resource<Boolean> | ApiErrorResponse> {
-        const url = `/officer-filing/company/${companyNumber}/eligibility-check/past-future-dissolved`;
-        const resp: HttpResponse = await this.client.httpGet(url);
-
-        if (resp.status >= 400) {
-            return { httpStatusCode: resp.status, errors: [resp.error] };
-        }
-
-        return { httpStatusCode: resp.status, resource: resp.body as Boolean };
-    }
 }
+
+
