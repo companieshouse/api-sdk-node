7c8648bc1b11d3c0f2941eecbb35c45760d1b308
jbestwickch <jbestwick@companieshouse.gov.uk>
2023-05-11 14:59:52 +0100

    Add dist folder during dev to allow for local testing

---
+++ dist/services/account-validator/types.d.ts
@@ -0,0 +1,22 @@
+export interface AccountValidatorRequest {
+    fileName: string;
+    id: string;
+}
+export interface Data {
+    balanceSheetDate: string;
+    accountsType: string;
+    companieshouseRegisteredNumber: string;
+}
+export interface Result {
+    errorMessages: string[];
+    data: Data;
+    validationStatus: ValidationStatus;
+}
+export interface AccountValidatorResponse {
+    status: RequestStatus;
+    result: Result;
+    fileId: string;
+}
+declare type RequestStatus = "complete" | "pending";
+declare type ValidationStatus = "OK" | "FAILED";
+export {};
