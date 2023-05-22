fb0a69c23dd65a9719d20de85bcc31487a8562d5
AndreKramer7654 <akramer@companieshouse.gov.uk>
2023-05-22 09:06:50 +0100

    test assert equal not one of

--- test/services/payment/service.spec.ts
+++ test/services/payment/service.spec.ts
@@ -66,7 +66,7 @@ describe("payment service", () => {
             const paymentService: PaymentService = new PaymentService(requestClient);
             const response = await paymentService.createPayment(mockRequestBody);
             const data = response.value as ApiResponse<Payment>;
-            expect(data.httpStatusCode).to.be.oneOf([401]);
+            expect(data.httpStatusCode).to.equal(401);
         });
 
         it("maps the payment fields", async () => {
