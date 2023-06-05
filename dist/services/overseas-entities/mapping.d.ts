013af3d1c34c990570a7c4f6b21cf21e37b3d671
jbestwickch <jbestwick@companieshouse.gov.uk>
2023-06-05 15:31:47 +0100

    Add dist for local testing

---
+++ dist/services/overseas-entities/mapping.d.ts
@@ -0,0 +1,3 @@
+import { OverseasEntity, OverseasEntityResource } from "./types";
+export declare const mapOverseasEntity: (body: OverseasEntity) => OverseasEntityResource;
+export declare const mapOverseasEntityResource: (body: OverseasEntityResource) => OverseasEntity;
