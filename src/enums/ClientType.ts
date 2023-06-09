d0aa75aa79c4eb830e7456470f4cb4c3a208891f
Moses Wejuli <mwejuli@companieshouse.gov.uk>
2023-06-09 13:37:00 +0100

    remove DEFAULT entry

--- src/enums/ClientType.ts
+++ src/enums/ClientType.ts
@@ -2,6 +2,5 @@ export enum ClientType {
     PUBLIC_WITH_OAUTH_TOKEN = "public_with_oauth_token",
     PRIVATE_WITH_OAUTH_TOKEN = "private_with_oauth_token",
     PUBLIC_WITH_API_KEY = "public_with_api_key",
-    PRIVATE_WITH_API_KEY = "private_with_api_key",
-    DEFAULT = "not-set"
+    PRIVATE_WITH_API_KEY = "private_with_api_key"
 };
