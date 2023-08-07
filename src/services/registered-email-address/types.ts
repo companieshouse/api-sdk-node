55c178ab17d9581580418e5356fac682b6c1794e
adeharrison-ch <aharrison1@companieshouse.gov.uk>
2023-08-07 14:10:27 +0100

    New type to handle full postRegisteredEmailAddress response

--- src/services/registered-email-address/types.ts
+++ src/services/registered-email-address/types.ts
@@ -1,13 +1,51 @@
 /**
- * RegisteredEmailAddressResource is what is returned from the api.
+ * RegisteredEmailAddressResource - what is sent to the api.
  */
 export interface RegisteredEmailAddressResource {
     registered_email_address: string;
-    accept_appropriate_email_address_statement: string;
+    accept_appropriate_email_address_statement: boolean;
 }
 
-// response resource
+/**
+ * RegisteredEmailAddressResponse - what is returned from the api.
+ */
+export interface RegisteredEmailAddressResponse {
+    id: string,
+    data: {
+        registered_email_address: string;
+        accept_appropriate_email_address_statement: boolean;
+        etag: string,
+        kind: string
+    }
+    created_at: string,
+    updated_at: string,
+    links: {
+        self: string
+    }
+}
+
+/**
+ * RegisteredEmailAddressCreated - transposed api response data for web use.
+ */
+export interface RegisteredEmailAddressCreatedResource {
+    id: string,
+    data: {
+        registeredEmailAddress: string;
+        acceptAppropriateEmailAddressStatement: boolean;
+        etag: string,
+        kind: string
+    }
+    createdAt: string,
+    updatedAt: string,
+    links: {
+        self: string;
+    }
+}
+
+/**
+ * RegisteredEmailAddress holds registered email address for web use.
+ */
 export interface RegisteredEmailAddress {
     registeredEmailAddress: string;
-    acceptAppropriateEmailAddressStatement: string;
+    acceptAppropriateEmailAddressStatement: boolean;
 }
