448c2fa552b5163aff54c3527811188fc6752e03
jbestwickch <jbestwick@companieshouse.gov.uk>
2023-09-07 11:34:10 +0100

    Remove dist folder now local dev is complete

--- dist/services/bankrupt-officer/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BadosService = void 0;
-var scottish_1 = require("./scottish");
-Object.defineProperty(exports, "BadosService", { enumerable: true, get: function () { return scottish_1.BadosService; } });
-//# sourceMappingURL=index.js.map
--- dist/services/search/dissolved-search/service.d.ts
+++
@@ -1,8 +0,0 @@
-import { IHttpClient } from "../../../http";
-import { CompaniesResource } from "./types";
-import Resource from "../../resource";
-export default class DissolvedSearchService {
-    private readonly client;
-    constructor(client: IHttpClient);
-    getCompanies(companyName: string, requestId: string, searchType: string, startIndex: number | null, searchBefore: string | null, searchAfter: string | null, size: number | null): Promise<Resource<CompaniesResource>>;
-}
