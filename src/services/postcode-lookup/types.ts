35693abc74ebb71c14177fa5d344ed7cadf530c9
Milan Desai <mdesai@companieshouse.gov.uk>
2023-09-21 11:44:56 +0100

    Created postcode lookup service.

---
+++ src/services/postcode-lookup/types.ts
@@ -0,0 +1,8 @@
+export interface UKAddresses {
+    postcode: string,
+    premise: string,
+    addressLine1: string,
+    addressLine2?: string,
+    postTown: string,
+    country: string
+}
---
+++ test/services/postcode-lookup/postcode.lookup.mock.ts
@@ -0,0 +1,3 @@
+import { RequestClient } from "../../../src";
+
+export const requestClient = new RequestClient({ baseUrl: "URL_NOT_USED", oauthToken: "TOKEN_NOT_USED" });
