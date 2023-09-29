a760cb846102c857a9bcdf1b0bca0d559dbf9e72
Milan Desai <mdesai@companieshouse.gov.uk>
2023-09-29 15:29:45 +0100

    DACT-736 introduction of new postcode look up service

---
+++ src/services/postcode-lookup/service.ts
@@ -0,0 +1,35 @@
+import { HttpResponse, IHttpClient } from "../../http";
+import Resource from "../resource";
+import { UKAddresses } from "./types";
+import Mapping from "../../mapping/mapping";
+import Util from "../psc-discrepancies-report/util"
+
+export default class PostcodeLookupService {
+    constructor (readonly client: IHttpClient) { }
+
+    public async isValidUKPostcode (postcodeValidationUrl: string, postcode: string): Promise<boolean> {
+        const url = `${postcodeValidationUrl}/${postcode}`;
+        return this.getValidatePostcodeLookupResponse(url);
+    }
+
+    public async getListOfValidPostcodeAddresses (postcodeAddressesLookupUrl: string, postcode: string): Promise<Resource<UKAddresses[]>> {
+        const url = `${postcodeAddressesLookupUrl}/${postcode}`;
+        return this.getPostcodeAddressesLookup(url);
+    }
+
+    private async getValidatePostcodeLookupResponse (url: string): Promise<boolean> {
+        const resp: HttpResponse = await this.client.httpGet(url);
+        return resp.status === 200;
+    }
+
+    private async getPostcodeAddressesLookup (url: string): Promise<Resource<UKAddresses[]>> {
+        const resp: HttpResponse = await this.client.httpGet(url);
+        if (resp.status !== 200) {
+            return { httpStatusCode: resp.status, resource: {} as UKAddresses[] };
+        }
+        const resource: Resource<UKAddresses[]> = { httpStatusCode: resp.status, resource: null };
+        const body = resp.body as UKAddresses[];
+        resource.resource = Mapping.camelCaseKeys<UKAddresses[]>(body)
+        return resource;
+    }
+}
---
+++ test/services/postcode-lookup/service.spec.ts
@@ -0,0 +1,104 @@
+import sinon from "sinon";
+import { PostcodeLookupService, UKAddresses } from "../../../src/services/postcode-lookup";
+import { RequestClient } from "../../../src";
+import { expect } from "chai";
+
+const requestClient = new RequestClient({ baseUrl: "URL_NOT_USED", oauthToken: "NOT USED" });
+const mockResponseBodyOfUKAddress1: UKAddresses = ({
+    premise: "123",
+    addressLine1: "123 Main St",
+    postTown: "London",
+    postcode: "SW1A 1AA",
+    country: "GB-ENG"
+});
+const mockResponseBodyOfUKAddress2: UKAddresses = ({
+    premise: "125",
+    addressLine1: "123 Main St",
+    postTown: "London",
+    postcode: "SW1A 1AA",
+    country: "GB-ENG"
+});
+const mockResponseBody: UKAddresses[] = [
+    mockResponseBodyOfUKAddress1,
+    mockResponseBodyOfUKAddress2
+]
+
+describe("test isValidUKPostcode", () => {
+    it("should return true for a valid postcode", async () => {
+        const mockGetResponse = {
+            status: 200,
+            body: mockResponseBody
+        }
+        const mockRequest = sinon.stub(requestClient, "httpGet").returns(Promise.resolve(mockGetResponse));
+        const postcode = "SW1A1AA";
+        const postcodeValidationUrl = "https://example.com/postcode";
+        const postcodeLookupService: PostcodeLookupService = new PostcodeLookupService(requestClient);
+        const result = await postcodeLookupService.isValidUKPostcode(postcodeValidationUrl, postcode);
+        expect(result).to.be.equal(true);
+    });
+
+    it("should return false for an invalid postcode", async () => {
+        const mockGetResponse = {
+            status: 404,
+            body: null
+        }
+        const mockRequest = sinon.stub(requestClient, "httpGet").returns(Promise.resolve(mockGetResponse));
+        const postcode = "SW1A1AB";
+        const postcodeValidationUrl = "https://example.com/postcode";
+        const postcodeLookupService: PostcodeLookupService = new PostcodeLookupService(requestClient);
+        const result = await postcodeLookupService.isValidUKPostcode(postcodeValidationUrl, postcode);
+        expect(result).to.be.equal(false);
+    });
+});
+describe("test getListOfValidPostcodeAddresses", () => {
+    it("should return a list of addresses for a valid postcode", async () => {
+        const mockGetResponse = {
+            status: 200,
+            body: mockResponseBody
+        }
+        const mockRequest = sinon.stub(requestClient, "httpGet").returns(Promise.resolve(mockGetResponse));
+        const postcode = "SW1A1AA";
+        const postcodeAddressesLookupUrl = "https://example.com/multiple-addresses";
+        const postcodeLookupService: PostcodeLookupService = new PostcodeLookupService(requestClient);
+        const result = await postcodeLookupService.getListOfValidPostcodeAddresses(postcodeAddressesLookupUrl, postcode);
+        expect(mockRequest).to.have.been.calledOnce;
+        expect(result.httpStatusCode).to.be.equal(200);
+        expect(result.resource).to.not.be.undefined;
+        expect(result.resource?.length).to.be.equal(2);
+        expect(JSON.stringify(result.resource![0])).to.be.equals(JSON.stringify(mockResponseBodyOfUKAddress1));
+        expect(JSON.stringify(result.resource![1])).to.be.equals(JSON.stringify(mockResponseBodyOfUKAddress2));
+    });
+
+    it("should return an empty list for an invalid postcode", async () => {
+        const mockGetResponse = {
+            status: 404,
+            body: null
+        }
+        const mockRequest = sinon.stub(requestClient, "httpGet").returns(Promise.resolve(mockGetResponse));
+        const postcode = "SW1A1AB";
+        const postcodeAddressesLookupUrl = "https://example.com/multiple-addresses";
+        const postcodeLookupService: PostcodeLookupService = new PostcodeLookupService(requestClient);
+        const result = await postcodeLookupService.getListOfValidPostcodeAddresses(postcodeAddressesLookupUrl, postcode);
+        expect(mockRequest).to.have.been.calledOnce;
+        expect(result.httpStatusCode).to.be.equal(404);
+        expect(result.resource).to.not.be.undefined;
+        expect(result.resource).to.be.empty
+    });
+    it("test", async () => {
+        const mockGetResponse = {
+            status: 200,
+            body: mockResponseBody
+        }
+        const mockRequest = sinon.stub(requestClient, "httpGet").returns(Promise.resolve(mockGetResponse));
+        const postcode = "IM24NN";
+        const postcodeAddressesLookupUrl = "http://postcode.cidev.aws.chdev.org/multiple-addresses";
+        const postcodeLookupService: PostcodeLookupService = new PostcodeLookupService(requestClient);
+        const result = await postcodeLookupService.getListOfValidPostcodeAddresses(postcodeAddressesLookupUrl, postcode);
+        expect(mockRequest).to.have.been.calledOnce;
+        expect(result.httpStatusCode).to.be.equal(200);
+        expect(result.resource).to.not.be.undefined;
+        expect(result.resource?.length).to.be.equal(2);
+        expect(JSON.stringify(result.resource![0])).to.be.equals(JSON.stringify(mockResponseBodyOfUKAddress1));
+        expect(JSON.stringify(result.resource![1])).to.be.equals(JSON.stringify(mockResponseBodyOfUKAddress2));
+    });
+});
