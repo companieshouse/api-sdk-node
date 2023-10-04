1dc2b978b4726396367e92304a3b789d2c8c4b56
Milan Desai <mdesai@companieshouse.gov.uk>
2023-10-04 15:01:00 +0100

    add dist for test

--- dist/services/officer-filing/service.js
+++ dist/services/officer-filing/service.js
@@ -151,6 +151,7 @@ class default_1 {
             nationality2_link: officerFiling.nationality2Link,
             nationality3_link: officerFiling.nationality3Link,
             residential_address: this.mapAddressToDto(officerFiling.residentialAddress),
+            residentialAddressBackLink: officerFiling.residentialAddressBackLink,
             service_address: this.mapAddressToDto(officerFiling.serviceAddress)
         };
     }
@@ -196,6 +197,7 @@ class default_1 {
             nationality2Link: officerFilingDto.nationality2_link,
             nationality3Link: officerFilingDto.nationality3_link,
             residentialAddress: this.mapAddressFromDto(officerFilingDto.residential_address),
+            residentialAddressBackLink: officerFilingDto.residentialAddressBackLink,
             serviceAddress: this.mapAddressFromDto(officerFilingDto.service_address)
         };
     }
