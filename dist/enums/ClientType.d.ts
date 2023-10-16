27808dea6db1b4199c570101b58719d1521ccb1f
anona-w <117649383+anona-w@users.noreply.github.com>
2023-10-16 13:48:06 +0100

    Adding dist files

---
+++ dist/enums/ClientType.d.ts
@@ -0,0 +1,6 @@
+export declare enum ClientType {
+    PUBLIC_WITH_OAUTH_TOKEN = "public_with_oauth_token",
+    PRIVATE_WITH_OAUTH_TOKEN = "private_with_oauth_token",
+    PUBLIC_WITH_API_KEY = "public_with_api_key",
+    PRIVATE_WITH_API_KEY = "private_with_api_key"
+}
