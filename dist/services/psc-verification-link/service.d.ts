0a9682134186036b4f82651293e85348bd37a071
Simon Boyd <sboyd@companieshouse.gov.uk>
2024-04-02 10:46:11 +0100

    Remove dist/

--- dist/services/psc-verification-link/service.d.ts
+++
@@ -1,9 +0,0 @@
-import { PscVerification, PscVerificationResource } from "./types";
-import { IHttpClient } from "../../http";
-import Resource, { ApiErrorResponse } from "../resource";
-export default class {
-    private readonly client;
-    constructor(client: IHttpClient);
-    postPscVerification(transactionId: string, pscVerification: PscVerification): Promise<Resource<PscVerificationResource> | ApiErrorResponse>;
-    private populateResource;
-}
