961f35f4437fbc3e25803f734d13003ee039b58c
swalke-ch <swalke@companieshouse.gov.uk>
2024-07-30 11:32:20 +0100

    updated endpoint urls for acsp

--- src/services/acsp/service.ts
+++ src/services/acsp/service.ts
@@ -10,8 +10,8 @@ import Mapping from "../../mapping/mapping";
 export default class {
     constructor (private readonly client: IHttpClient) { }
 
-    public async getAcsp (transactionId: string, id: string): Promise<Resource<AcspData> | ApiErrorResponse> {
-        const url = `/transactions/${transactionId}/acsp/${id}`;
+    public async getAcsp (transactionId: string, acspApplicationId: string): Promise<Resource<AcspData> | ApiErrorResponse> {
+        const url = `/transactions/${transactionId}/authorised-corporate-service-provider-applications/${acspApplicationId}`;
         const resp: HttpResponse = await this.client.httpGet(url);
 
         if (resp.status >= 400) {
@@ -34,7 +34,7 @@ export default class {
      * @param acsp the data to be saved
      */
     public async postACSP (transactionId: string, acsp: AcspData): Promise<Resource<AcspResponse> | ApiErrorResponse> {
-        const url = `/transactions/${transactionId}/acsp`;
+        const url = `/transactions/${transactionId}/authorised-corporate-service-provider-applications`;
 
         const resp = await this.client.httpPost(url, acsp);
 
@@ -51,10 +51,11 @@ export default class {
     /**
      * Put an ACSP object to update on the API.
      * @param transactionId transaction id associated with the data to be saved
+     * @param acspApplicationId acsp application id unique to the application
      * @param acsp the data to be saved
      */
-    public async putACSP (transactionId: string, acsp: AcspData): Promise<Resource<AcspResponse> | ApiErrorResponse> {
-        const url = `/transactions/${transactionId}/acsp`;
+    public async putACSP (transactionId: string, acspApplicationId: string, acsp: AcspData): Promise<Resource<AcspResponse> | ApiErrorResponse> {
+        const url = `/transactions/${transactionId}/authorised-corporate-service-provider-applications${acspApplicationId}`;
 
         const resp = await this.client.httpPut(url, acsp);
 
@@ -69,30 +70,20 @@ export default class {
     }
 
     /* get saved application */
-    public async getSavedApplication (userId: string): Promise<HttpResponse> {
-        const url = `/acsp-api/user/${userId}/application`;
+    public async getSavedApplication (acspApplicationId: string): Promise<HttpResponse> {
+        const url = `/acsp-api/user/${acspApplicationId}/application`;
         const resp: HttpResponse = await this.client.httpGet(url);
         return resp;
     }
 
     /**
      * Delete an existing ACSP application from MongoDB
-     * @param userId the id of the user whose application will be deleted
+     * @param acspApplicationId the id of the user whose application will be deleted
      */
-    public async deleteSavedApplication (userId: string): Promise<HttpResponse> {
-        const url = `/acsp-api/user/${userId}/application`;
+    public async deleteSavedApplication (acspApplicationId: string): Promise<HttpResponse> {
+        const url = `/acsp-api/user/${acspApplicationId}/application`;
         const resp: HttpResponse = await this.client.httpDelete(url);
         return resp;
     }
-
-    /**
-     * Send a confirmation email for application complete
-     * @param userId the id of the user who will recieve the email
-     * @param applicationReference the reference number of the application
-     */
-    public async sendConfirmationEmail (userId: string, applicationReference: string): Promise<HttpResponse> {
-        const url = `/acsp-api/user/${userId}/application-submit/${applicationReference}`;
-        const resp: HttpResponse = await this.client.httpPost(url);
-        return resp;
-    }
+    
 }
