d8a30c0faf95c3b7f0eea2fe208aebc3725657d6
eedwards0 <eedwards2@companieshouse.gov.uk>
2024-12-11 12:42:04 +0000

    Remove contents of dist

--- dist/services/limited-partnerships/service.d.ts
+++
@@ -1,8 +0,0 @@
-import { IHttpClient } from "../../http";
-import { LimitedPartnership, LimitedPartnershipCreated } from "./types";
-import Resource, { ApiErrorResponse } from "../resource";
-export default class LimitedPartnershipsService {
-    private readonly client;
-    constructor(client: IHttpClient);
-    postLimitedPartnership(transactionId: string, body: LimitedPartnership): Promise<Resource<LimitedPartnershipCreated> | ApiErrorResponse>;
-}
--- dist/services/postcode-lookup/types.d.ts
+++
@@ -1,8 +0,0 @@
-export interface UKAddress {
-    postcode: string;
-    premise: string;
-    addressLine1: string;
-    addressLine2?: string;
-    postTown: string;
-    country: string;
-}
