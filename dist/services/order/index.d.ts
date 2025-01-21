0f84638f49e9cf12c0462fef7c6307bfcdf01a4f
eedwards0 <eedwards2@companieshouse.gov.uk>
2025-01-21 12:29:36 +0000

    Remove contents of dist directory

--- dist/services/order/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { default as BasketService } from "./basket/service";
-export { default as CheckoutService } from "./checkout/service";
-export { default as OrderService } from "./order/service";
-export { default as CertificateService } from "./certificates/service";
-export { default as CertifiedCopiesService } from "./certified-copies/service";
-export { default as MidService } from "./mid/service";
