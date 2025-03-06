2da9abc3f4e208dd3e148549058735798e16b4f2
PhilipGlover <pglover@companieshouse.gov.uk>
2025-03-06 14:32:54 +0000

    add dist files whilst dev-ing, to be removed later

---
+++ dist/services/psc/types.js
@@ -0,0 +1,20 @@
+"use strict";
+/**
+ * PersonWithSignificantControlResource is what is returned from the api.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+;
+;
+;
+;
+;
+;
+;
+;
+;
+;
+;
+;
+;
+;
+//# sourceMappingURL=types.js.map
