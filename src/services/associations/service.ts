712108e5ef04c29b13819733ac505bc9fa7ca200
mmodzelewski-ch <mmodzelewski@companieshouse.gov.uk>
2025-07-14 15:53:36 +0100

    SIV-461 User email parameter removed

--- src/services/associations/service.ts
+++ src/services/associations/service.ts
@@ -31,7 +31,6 @@ export default class AssociationsService {
      * @param includeRemoved - a flag to get a list of companies where status is "removed". Default value: false.
      * @param pageIndex - a page number to be returned. Default value: 0.
      * @param itemsPerPage - a number of items to be returned per page. Default value: 15.
-     * @param userEmail - an email address of a user to check if associated with the company.
      * @param userId - a unique identifier of a user to check if associated with the company.
      * @returns a promise that resolves to the HTTP response from the server that includes the associations or errors object.
      */
@@ -40,7 +39,6 @@ export default class AssociationsService {
         includeRemoved?: boolean,
         pageIndex?: number,
         itemsPerPage?: number,
-        userEmail?: string,
         userId?: string
     ): Promise<Resource<AssociationList | Errors>> {
         let queryString: string = "";
@@ -54,13 +52,8 @@ export default class AssociationsService {
             queryString = this.getQueryString(queryParameters);
         }
 
-        let headers: Headers;
-        if (userEmail) {
-            headers = { user_email: userEmail };
-        }
-
         const url = `/associations/companies/${companyNumber}${queryString}`;
-        const response = await this.client.httpGet(url, headers);
+        const response = await this.client.httpGet(url);
 
         return this.getResource(response) as Resource<AssociationList | Errors>;
     }
