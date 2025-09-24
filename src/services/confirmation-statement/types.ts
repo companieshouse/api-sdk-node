05dbb8b0a715d23718c90f120a5ddd7ab27aed96
dho-ch <dho@companieshouse.gov.uk>
2025-09-24 17:57:10 +0100

    CSE-680: ACSP - Confirmation Statement Date - Transaction Update

--- src/services/confirmation-statement/types.ts
+++ src/services/confirmation-statement/types.ts
@@ -25,7 +25,8 @@ export interface ConfirmationStatementSubmissionDataResource {
     sic_code_data?: SicCodeDataResource,
     statement_of_capital_data?: StatementOfCapitalDataResource,
     trading_status_data?: TradingStatusDataResource,
-    accept_lawful_purpose_statement?: boolean
+    accept_lawful_purpose_statement?: boolean,
+    new_confirmation_date?: string | null
 }
 
 export interface ConfirmationStatementSubmissionData {
@@ -39,7 +40,8 @@ export interface ConfirmationStatementSubmissionData {
     sicCodeData?: SicCodeData,
     statementOfCapitalData?: StatementOfCapitalData,
     tradingStatusData?: TradingStatusData,
-    acceptLawfulPurposeStatement?: boolean
+    acceptLawfulPurposeStatement?: boolean,
+    newConfirmationDate?: string | null
 }
 
 export interface ConfirmationStatementSubmissionSectionResource {
