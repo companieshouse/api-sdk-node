64fa7a8b474b73fbbbc9d448f1faad4008c4a599
PhilipGlover <pglover@companieshouse.gov.uk>
2025-11-05 10:52:26 +0000

    now PR approved, removing dist files.  Also update to a test I'd missed

--- dist/services/sic-code/service.d.ts
+++
@@ -1,8 +0,0 @@
-import { CondensedSicCodeData } from "./types";
-import { IHttpClient } from "../../http";
-import Resource from "../resource";
-export default class {
-    private readonly client;
-    constructor(client: IHttpClient);
-    getCondensedSicCodes(): Promise<Resource<CondensedSicCodeData[]>>;
-}
