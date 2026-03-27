77e773f65d6d68e8bba43ace466327b15936779f
clewis1 <clewis1@companieshouse.gov.uk>
2026-03-27 16:04:06 +0000

    Add get verification by id function

--- dist/services/accounts-filing/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AccountsFilingService = void 0;
-var service_1 = require("./service");
-Object.defineProperty(exports, "AccountsFilingService", { enumerable: true, get: function () { return service_1.AccountsFilingService; } });
-//# sourceMappingURL=index.js.map
--- dist/services/search/alphabetical-search/service.d.ts
+++
@@ -1,8 +0,0 @@
-import { IHttpClient } from "../../../http";
-import { CompaniesResource } from "./types";
-import Resource from "../../resource";
-export default class AlphabeticalSearchService {
-    private readonly client;
-    constructor(client: IHttpClient);
-    getCompanies(companyName: string, requestId: string, searchBefore: string | null, searchAfter: string | null, size: number | null): Promise<Resource<CompaniesResource>>;
-}
