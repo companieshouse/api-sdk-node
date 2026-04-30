3a751e1e8904035c12043b819c011a64694b3e2d
artemisgardnerch <agardner@companieshouse.gov,uk>
2026-04-30 11:50:04 +0100

    fixing indentation issues

--- src/services/search/advanced-search/service.ts
+++ src/services/search/advanced-search/service.ts
@@ -3,23 +3,21 @@ import { CompaniesResource } from "./types";
 import Resource from "../../resource";
 import "url-search-params-polyfill";
 
-
-
 export default class AdvancedSearchService {
 
-    START_INDEX_QUERY = "start_index";
-    COMPANY_NAME_INCLUDES_QUERY = "company_name_includes";
-    COMPANY_NAME_EXCLUDES_QUERY = "company_name_excludes"
-    LOCATION_QUERY = "location";
-    INCORPORATED_FROM_QUERY = "incorporated_from";
-    INCORPORATED_TO_QUERY = "incorporated_to";
-    SIC_CODES_QUERY = "sic_codes";
-    COMPANY_STATUS_QUERY = "company_status";
-    COMPANY_TYPE_QUERY = "company_type";
-    COMPANY_SUBTYPE_QUERY = "company_subtype";
-    DISSOLVED_FROM_QUERY_PARAMETER = "dissolved_from";
-    DISSOLVED_TO_QUERY_PARAMETER = "dissolved_to";
-    SIZE_QUERY_PARAMETER = "size";
+START_INDEX_QUERY = "start_index";
+COMPANY_NAME_INCLUDES_QUERY = "company_name_includes";
+COMPANY_NAME_EXCLUDES_QUERY = "company_name_excludes"
+LOCATION_QUERY = "location";
+INCORPORATED_FROM_QUERY = "incorporated_from";
+INCORPORATED_TO_QUERY = "incorporated_to";
+SIC_CODES_QUERY = "sic_codes";
+COMPANY_STATUS_QUERY = "company_status";
+COMPANY_TYPE_QUERY = "company_type";
+COMPANY_SUBTYPE_QUERY = "company_subtype";
+DISSOLVED_FROM_QUERY_PARAMETER = "dissolved_from";
+DISSOLVED_TO_QUERY_PARAMETER = "dissolved_to";
+SIZE_QUERY_PARAMETER = "size";
 
     constructor (private readonly client: IHttpClient) { }
     public async getCompanies (startIndex: number | null, companyNameIncludes: string | null, companyNameExcludes: string | null, location: string | null, incorporatedFrom: string | null,
@@ -32,7 +30,7 @@ export default class AdvancedSearchService {
         }
 
         const advancedSearchUrl = this.buildURL(startIndex, companyNameIncludes, companyNameExcludes, location, incorporatedFrom, incorporatedTo, sicCodes, companyStatus, companyType,
-                                                    companySubtype, dissolvedFrom, dissolvedTo, size, "/advanced-search/companies?");
+            companySubtype, dissolvedFrom, dissolvedTo, size, "/advanced-search/companies?");
 
         const resp = await this.client.httpGet(advancedSearchUrl, additionalHeaders);
 
@@ -79,60 +77,58 @@ export default class AdvancedSearchService {
         incorporatedTo: string | null, sicCodes: string | null, companyStatus: string | null, companyType: string | null, companySubtype: string | null, dissolvedFrom: string | null,
         dissolvedTo: string | null, size: number | null, baseUrl: string): string {
 
-            const buildAdvancedSearchURL = new URLSearchParams(baseUrl)
+        const buildAdvancedSearchURL = new URLSearchParams(baseUrl)
+        if (startIndex !== null) {
+            buildAdvancedSearchURL.append(this.START_INDEX_QUERY, String(startIndex));
+        }
 
-            if (startIndex !== null) {
-                buildAdvancedSearchURL.append(this.START_INDEX_QUERY, String(startIndex));
-            }
-    
-            if (companyNameIncludes !== null) {
-                buildAdvancedSearchURL.append(this.COMPANY_NAME_INCLUDES_QUERY, companyNameIncludes)
-            }
-    
-            if (companyNameExcludes !== null) {
-                buildAdvancedSearchURL.append(this.COMPANY_NAME_EXCLUDES_QUERY, companyNameExcludes)
-            }
-    
-            if (location !== null) {
-                buildAdvancedSearchURL.append(this.LOCATION_QUERY, location)
-            }
-    
-            if (incorporatedFrom !== null) {
-                buildAdvancedSearchURL.append(this.INCORPORATED_FROM_QUERY, incorporatedFrom)
-            }
-    
-            if (incorporatedTo !== null) {
-                buildAdvancedSearchURL.append(this.INCORPORATED_TO_QUERY, incorporatedTo)
-            }
-    
-            if (sicCodes !== null) {
-                buildAdvancedSearchURL.append(this.SIC_CODES_QUERY, sicCodes)
-            }
-    
-            if (companyStatus !== null) {
-                buildAdvancedSearchURL.append(this.COMPANY_STATUS_QUERY, companyStatus);
-            }
-    
-            if (companyType !== null) {
-                buildAdvancedSearchURL.append(this.COMPANY_TYPE_QUERY, companyType)
-            }
-    
-            if (companySubtype !== null) {
-                buildAdvancedSearchURL.append(this.COMPANY_SUBTYPE_QUERY, companySubtype)
-            }
-    
-            if (dissolvedFrom !== null) {
-                buildAdvancedSearchURL.append(this.DISSOLVED_FROM_QUERY_PARAMETER, dissolvedFrom)
-            }
-    
-            if (dissolvedTo !== null) {
-                buildAdvancedSearchURL.append(this.DISSOLVED_TO_QUERY_PARAMETER, dissolvedTo)
-            }
-    
-            if (size !== null) {
-                buildAdvancedSearchURL.append(this.SIZE_QUERY_PARAMETER, String(size))
-            }
+        if (companyNameIncludes !== null) {
+            buildAdvancedSearchURL.append(this.COMPANY_NAME_INCLUDES_QUERY, companyNameIncludes)
+        }
 
-            return buildAdvancedSearchURL.toString();
+        if (companyNameExcludes !== null) {
+            buildAdvancedSearchURL.append(this.COMPANY_NAME_EXCLUDES_QUERY, companyNameExcludes)
+        }
+
+        if (location !== null) {
+            buildAdvancedSearchURL.append(this.LOCATION_QUERY, location)
+        }
+
+        if (incorporatedFrom !== null) {
+            buildAdvancedSearchURL.append(this.INCORPORATED_FROM_QUERY, incorporatedFrom)
+        }
+
+        if (incorporatedTo !== null) {
+            buildAdvancedSearchURL.append(this.INCORPORATED_TO_QUERY, incorporatedTo)
+        }
+
+        if (sicCodes !== null) {
+            buildAdvancedSearchURL.append(this.SIC_CODES_QUERY, sicCodes)
+        }
+
+        if (companyStatus !== null) {
+            buildAdvancedSearchURL.append(this.COMPANY_STATUS_QUERY, companyStatus);
+        }
+
+        if (companyType !== null) {
+            buildAdvancedSearchURL.append(this.COMPANY_TYPE_QUERY, companyType)
+        }
+
+        if (companySubtype !== null) {
+            buildAdvancedSearchURL.append(this.COMPANY_SUBTYPE_QUERY, companySubtype)
+        }
+
+        if (dissolvedFrom !== null) {
+            buildAdvancedSearchURL.append(this.DISSOLVED_FROM_QUERY_PARAMETER, dissolvedFrom)
+        }
+
+        if (dissolvedTo !== null) {
+            buildAdvancedSearchURL.append(this.DISSOLVED_TO_QUERY_PARAMETER, dissolvedTo)
+        }
+
+        if (size !== null) {
+            buildAdvancedSearchURL.append(this.SIZE_QUERY_PARAMETER, String(size))
+        }
+        return buildAdvancedSearchURL.toString();
         }
 }
