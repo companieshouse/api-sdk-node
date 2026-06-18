8b0b79a0a6026a7cef3d8d90a2f5693c8de40c13
lduranteau <lduranteau@companieshouse.gov.uk>
2026-06-18 14:31:53 +0100

    [lp-2054] : add natures_of_control_types to limited partnerships test

--- test/services/limited-partnerships/limited.partnerships.mock.ts
+++ test/services/limited-partnerships/limited.partnerships.mock.ts
@@ -13,7 +13,8 @@ import {
     LimitedPartner,
     PersonWithSignificantControl,
     NaturesOfControl,
-    PersonWithSignificantControlType
+    PersonWithSignificantControlType,
+    NaturesOfControlTypes
 } from "../../../src/services/limited-partnerships";
 
 export const requestClient = new RequestClient({
@@ -210,6 +211,7 @@ export const PERSON_WITH_SIGNIFICANT_CONTROL_OBJECT_MOCK: PersonWithSignificantC
             postal_code: "SC15 1N2"
         },
         type: PersonWithSignificantControlType.INDIVIDUAL_PERSON,
+        natures_of_control_types: [NaturesOfControlTypes.INDIVIDUAL, NaturesOfControlTypes.FIRM, NaturesOfControlTypes.TRUST],
         completed: true
     }
 };
