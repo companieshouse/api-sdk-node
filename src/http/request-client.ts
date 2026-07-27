76220ffeefdc1d75cf0fe17aae64164f8383bc9e
NeilShadrach <nshadrach@companieshouse.gov.uk>
2026-07-27 11:08:16 +0100

    ASM-1563 Use startsWith consistently

--- src/http/request-client.ts
+++ src/http/request-client.ts
@@ -76,7 +76,7 @@ export default class RequestClient extends AbstractClient {
         if (uri.startsWith("http")) {
             return uri;
         }
-        if (uri.length > 0 && uri.charAt(0) !== "/") {
+        if (!uri.startsWith("/")) {
             uri = `/${uri}`;
         }
         if (uri === "/") {
